e8fa593046c9cbf1305c92fdcb2cc48dfd8e95ad
Giorgi Poplavski <you@example.com>
2024-01-09 22:10:27 -1200

    Homework MySQL

---
+++ autorental_car rental availability.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: autorental
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `car rental availability`
+--
+
+LOCK TABLES `car rental availability` WRITE;
+/*!40000 ALTER TABLE `car rental availability` DISABLE KEYS */;
+INSERT INTO `car rental availability` VALUES (1,'Toyota','Camry','Automatic',2.5,2014,'Black'),(2,'Toyota','Camry','Automatic',2.5,2014,'Black'),(3,'Toyota','Camry','Manual',3.5,2018,'Black'),(4,'Toyota','Camry','Automatic',2.5,2018,'Black'),(5,'Toyota','Camry','Automatic',2.5,2022,'Black'),(6,'Toyota','Corolla','Automatic',1.8,2014,'White'),(7,'Toyota','Corolla','Automatic',2.4,2014,'Black'),(8,'Toyota','Corolla','Manual',2.4,2018,'Black'),(9,'Toyota','Corolla','Automatic',3.2,2021,'Blue'),(10,'Toyota','Isis','Automatic',2.2,2016,'Black'),(11,'Toyota','Isis','Automatic',2.2,2016,'Gray'),(12,'Toyota','Land Cruiser','Automatic',4.5,2018,'Black'),(13,'Toyota','Land Cruiser','Automatic',4.5,2018,'Gray'),(14,'Toyota','Land Cruiser','Automatic',4.5,2022,'White'),(15,'Toyota','Hilux','Automatic',2.8,2020,'Blue'),(16,'Toyota','Hilux','Manual',2.8,2020,'Red');
+/*!40000 ALTER TABLE `car rental availability` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-09 22:05:06
